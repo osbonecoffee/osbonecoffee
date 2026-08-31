12acc6f1ab0c5467aca9d5c2220998912f91c859
osbonecoffee <osbonecoffee@gmail.com>
2026-08-31 07:24:59 +0700

    Update app-config.js

--- app-config.js
+++ app-config.js
@@ -1,4 +1,4 @@
 // URL Web App Google Apps Script.
 // Isi URL yang berakhiran /exec setelah deployment Apps Script selesai.
 // Contoh: https://script.google.com/macros/s/XXXXXXXXXXXX/exec
-window.APP_SCRIPT_URL = '';
+window.APP_SCRIPT_URL = 'https://script.google.com/macros/s/AKfycbxTIA59Vw6oK6rujxFv5YuW5GtqLQ2u7T1jxQaComc4htejhyaWeCVBd8xbUhlQM3q-/exec';
